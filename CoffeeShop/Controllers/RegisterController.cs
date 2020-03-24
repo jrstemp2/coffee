d8f174b5eada207f9946c9432a3d27c78300f726
John Stempowski <johnstempowski@quickenloans.com>
2020-03-24 17:01:41 -0400

    First Attempt

---
+++ CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("RegisterIndex");
+        }
+        [HttpPost]
+        public IActionResult Register(string fname, string lname, string email, string password, string offers)
+        {
+            ViewData["name"] = fname + " " + lname;
+            ViewData["email"] = email;
+            ViewData["offers"] = offers;
+            ViewData["password"] = password;
+            ViewData["username"] = fname[0] + lname;
+            return View();
+        }
+    }
+}
